3f3a819cdd8edaaadaa51857ff31a2d66424bccf
rwapp <r.whitaker@mac.com>
2019-12-05 16:36:31 +0000

    created cocoapod and added initial tests

---
+++ Example/A11yUITests_ExampleUITests/A11yUITests_ExampleUITests.swift
@@ -0,0 +1,40 @@
+//
+//  A11yUITests_ExampleUITests.swift
+//  A11yUITests_ExampleUITests
+//
+//  Created by Rob Whitaker on 05/12/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import A11yUITests
+
+@testable import A11yUITests_Example
+
+class A11yUITests_ExampleUITests: XCTestCase {
+
+    override func setUp() {
+        XCUIApplication().launch()
+    }
+
+    func test_allTests() {
+        runAllA11yTestsOnScreen()
+    }
+
+    func test_images() {
+        let images = XCUIApplication().images.allElementsBoundByIndex
+        run(a11yTests: imageA11yTestSuite(), on: images)
+    }
+
+    func test_buttons() {
+        let buttons = XCUIApplication().buttons.allElementsBoundByIndex
+
+        run(a11yTests: interactiveA11yTestSuite(), on: buttons)
+    }
+
+    func test_labels() {
+        let labels = XCUIApplication().staticTexts.allElementsBoundByIndex
+        run(a11yTests: labelA11yTestSuite(), on: labels)
+    }
+
+}
---
+++ Example/Pods/Target Support Files/Pods-A11yUITests_Example/Pods-A11yUITests_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_A11yUITests_Example : NSObject
+@end
+@implementation PodsDummy_Pods_A11yUITests_Example
+@end
