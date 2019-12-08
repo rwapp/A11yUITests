d1ef6c12231cc71b88301b50911dfa89ed94d672
rwapp <r.whitaker@mac.com>
2019-12-08 17:50:19 +0000

    a11yElement struct

---
+++ A11yElement.swift
@@ -0,0 +1,30 @@
+//
+//  A11yElement.swift
+//  A11yUITests
+//
+//  Created by Rob Whitaker on 08/12/2019.
+//
+
+import XCTest
+
+struct A11yElement {
+    let label: String
+    let frame: CGRect
+    let type: XCUIElement.ElementType
+
+    var isWindow: Bool {
+        return self.type == .window
+    }
+
+    var isInteractive: Bool {
+        // strictly switches, steppers, sliders, segmented controls, & text fields should be included
+        // but standard iOS implimentations aren't large enough.
+
+        return self.type == .button ||
+            self.type == .cell
+    }
+
+    var description: String {
+        return "\"\(self.label)\" \(self.type)"
+    }
+}
