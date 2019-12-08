edbb72ebecdc25a24359a9a2ca2bc65bc1b56860
rwapp <r.whitaker@mac.com>
2019-12-08 19:25:33 +0000

    formatting

--- Example/A11yUITests/AppDelegate.swift
+++ Example/A11yUITests/AppDelegate.swift
@@ -13,8 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    internal func application(_ application: UIApplication,
+                              didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
-
