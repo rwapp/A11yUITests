a7c019c3950793ccfe39416de61961887cfe1a93
rwapp <r.whitaker@mac.com>
2019-12-08 19:26:12 +0000

    type name in description

--- A11yElement.swift
+++ A11yElement.swift
@@ -26,6 +26,74 @@ struct A11yElement {
     }
 
     var description: String {
-        return "\"\(self.label)\" \(self.type)"
+        return "\"\(self.label)\" \(self.type.name())"
+    }
+}
+
+private extension XCUIElement.ElementType {
+    func name() -> String {
+        switch self {
+        case .staticText:
+            return "Label"
+        case .button:
+            return "Button"
+        case .textField:
+            return "Text Field"
+        case .cell:
+            return "Cell"
+        case .switch:
+            return "Switch"
+        case .window:
+            return "Window"
+        case .alert:
+            return "Alert"
+        case .keyboard:
+            return "Keyboard"
+        case .key:
+            return "Key"
+        case .pageIndicator:
+            return "Page Indicator"
+        case .activityIndicator:
+            return "Activity Indicator"
+        case .link:
+            return "Link"
+        case .searchField:
+            return "Search Field"
+        case .slider:
+            return "Slider"
+        case .textView:
+            return "Text View"
+        case .secureTextField:
+            return "Secure Text Field"
+        case .datePicker:
+            return "Date Picker"
+        case .map:
+            return "Map"
+        case .webView:
+            return "Web View"
+        case .stepper:
+            return "Stepper"
+        case .dialog:
+            return "Dialog"
+        case .tabBar:
+            return "Tab Bar"
+        case .statusBar:
+            return "Status Bar"
+        case .collectionView:
+            return "Collection View"
+        case .progressIndicator:
+            return "Progress Indicator"
+        case .segmentedControl:
+            return "Segmented Control"
+        case .picker:
+            return "Picker"
+        case .pickerWheel:
+            return "Picker Wheel"
+        case .image:
+            return "Image"
+
+        default:
+            return "Other"
+        }
     }
 }
